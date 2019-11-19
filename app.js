bf1c56d0b4aad80993bdb09c9c7c2081a1c475c4
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2019-11-19 14:45:16 +0100

    changed

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
+  burger.addEventListener("click", () => {
+    //Toggle navigation
+    nav.classList.toggle("nav-active");
 
-	burger.addEventListener('click', () => {
-		//Toggle navigation
-		nav.classList.toggle('nav-active');
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0s ease forwards ${index / 10 +
+          0.15}s`;
+      }
+    });
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
 
-		//Animate links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0s ease forwards ${index / 10 + 0.15}s`;
-			}
-		});
-		//Burger animation
-		burger.classList.toggle('toggle');
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("nav-active");
+    });
+  });
 
-	});
-
-	navLinks.forEach(link => {
-		link.addEventListener('click', () => {
-			nav.classList.remove('nav-active');
-		})
-	})
-
-	navLinks.forEach(link => {
-		link.addEventListener('click', () => {
-			burger.classList.toggle('toggle');
-		})
-	})
-
-
-}
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      burger.classList.toggle("toggle");
+    });
+  });
+};
 
 navSlide();
-
 
 /*
         const video = document.querySelector("video");
@@ -45,3 +41,28 @@ navSlide();
             video.play();
         }, 10000)
 */
+
+//Horizontal scroll
+const slider = document.querySelector(".slider");
+const dots = document.querySelectorAll(".dot");
+
+dots.forEach(dot => {
+  dot.addEventListener("click", () => {
+    const i = dot.dataset.index;
+    const sliderRect = slider.getBoundingClientRect();
+
+    slider.scrollLeft = sliderRect.width * i;
+  });
+});
+
+slider.addEventListener("scroll", event => {
+  console.log(slider.scrollLeft);
+  console.log(event);
+  // calculate percentage/index from left
+  // revmove active classname from dots
+  // add active state to one dot
+});
+
+// const dots = document.querySelectorAll(".dot");
+// event.target.classList.remove(".dot");
+// event.target.classList.add(".dotChanged");
